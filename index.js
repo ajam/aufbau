7910f66429a0830ce1442843986d8013c8a5027d
mhkeller <michael.keller@gmail.com>
2015-08-12 12:35:06 -0400

    support back / forward arrow nav

--- index.js
+++ index.js
@@ -48,8 +48,7 @@ app.on('activate-with-no-open-windows', function () {
 
 app.on('ready', function () {
 	var Menu = require('menu');
-	var MenuItem = require('menu-item');
-	var menu_template = require('./menus.js')
+	var menu_template = require('./menus.js')(BrowserWindow)
 	var menu = Menu.buildFromTemplate(menu_template);
 	Menu.setApplicationMenu(menu);
 
